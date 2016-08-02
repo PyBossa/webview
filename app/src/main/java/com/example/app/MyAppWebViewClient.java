793b55490ab761ce3bfcdaf88d87aeb670bc4101
Marvin Reimer <therealmarv@gmail.com>
2016-08-02 11:45:32 +0200

    Upgrade build version. Mosquito project webview example

--- app/src/main/java/com/example/app/MyAppWebViewClient.java
+++ app/src/main/java/com/example/app/MyAppWebViewClient.java
@@ -9,12 +9,14 @@ public class MyAppWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        if (Uri.parse(url).getHost().endsWith("example.com")) {
-            return false;
-        }
-
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        view.getContext().startActivity(intent);
-        return true;
+        // Never override URL loading
+        return false;
+//        if (Uri.parse(url).getHost().endsWith("example.com")) {
+//            return false;
+//        }
+//
+//        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+//        view.getContext().startActivity(intent);
+//        return true;
     }
 }
